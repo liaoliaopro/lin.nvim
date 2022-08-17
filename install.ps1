bad00418aebadf1bcadd08c1ed8cd697715c5d8a
linrongbin16 <linrongbin16@outlook.com>
2022-08-17 15:50:56 +0800

    save

--- install.ps1
+++ install.ps1
@@ -8,62 +8,62 @@ Function Message($msg) {
     Write-Host "[lin.nvim] - $msg"
 }
 
-Function TryBackup($src) {
-    If (Test-Path $src) {
+Function TryBackup($path) {
+    If (Test-Path $path) {
         $now=Get-Date -Format "yyyy-MM-dd.HH-mm-ss"
-        $target=-join($src, ".", $now)
-        Message("backup $src to $target")
-        Rename-Item $src $target
+        $target=-join($path, ".", $now)
+        Message "backup $path to $target"
+        Rename-Item $path $target
     }
 }
 
-Function TryDelete($src) {
-    If (Test-Path $src) {
-        (Get-Item $src).Delete()
+Function TryDelete($path) {
+    If (Test-Path $path) {
+        (Get-Item $path).Delete()
     }
 }
 
 Function InstallOrSkip($command, $target) {
     if (Get-Command -Name $target -ErrorAction SilentlyContinue) {
-        Message -msg "'${target}' already exist, skip..."
+        Message "'${target}' already exist, skip..."
     } else {
-        Message -msg "install '${target}' with command: '${command}'"
+        Message "install '${target}' with command: '${command}'"
         Invoke-Expression $command
     }
 }
 
 Function RustDependency() {
-    Message -msg "install modern rust commands with cargo"
+    Message "install modern rust commands with cargo"
     InstallOrSkip -command "cargo install ripgrep" -target "rg"
     InstallOrSkip -command "cargo install fd-find" -target "fd"
     InstallOrSkip -command "cargo install --locked bat" -target "bat"
 }
 
 Function Pip3Dependency() {
-    Message -msg "install python packages with pip3"
+    Message "install python packages with pip3"
     pip3 install pyOpenSSL neovim pynvim pep8 flake8 pylint yapf chardet cmakelang cmake-language-server click
 }
 
 Function NpmDependency() {
-    Message -msg "install node packages with npm"
+    Message "install node packages with npm"
     npm install -g yarn prettier neovim
 }
 
 Function InstallTemplates() {
-    Message -msg "install configurations from templates"
+    Message "install configurations from templates"
     Copy-Item $TEMPLATE_HOME\plugin-template.vim -Destination $VIM_HOME\plugin.vim
     Copy-Item $TEMPLATE_HOME\coc-settings-template.json -Destination $VIM_HOME\coc-settings.json
     Copy-Item $TEMPLATE_HOME\setting-template.vim -Destination $VIM_HOME\setting.vim
 }
 
 Function InstallVimrc() {
-    Message -msg "install .vimrc for vim"
+    Message "install .vimrc for vim"
     TryBackup "$env:USERPROFILE\_vimrc"
     cmd /c mklink %USERPROFILE%\_vimrc %USERPROFILE%\.vim\vimrc.vim
 }
 
 Function InstallNvimInit() {
-    Message -msg "install $NVIM_HOME and $NVIM_HOME\init.vim for neovim"
+    Message "install $NVIM_HOME and $NVIM_HOME\init.vim for neovim"
     TryBackup "$NVIM_HOME"
     TryDelete "$NVIM_HOME\init.vim"
     cmd /c mklink %USERPROFILE%\AppData\Local\nvim %USERPROFILE%\.vim
@@ -71,12 +71,12 @@ Function InstallNvimInit() {
 }
 
 Function InstallNvimPlugin() {
-    Message -msg "install neovim plugins"
+    Message "install neovim plugins"
     nvim -E -c "PlugInstall" -c "qall"
 }
 
 Function Main() {
-    Message -msg "install dependencies for windows"
+    Message "install dependencies for windows"
 
     # install dependencies
     RustDependency
